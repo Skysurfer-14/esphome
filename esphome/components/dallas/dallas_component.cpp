a6cb3865cf45bbf310eca5d23ba2f84751da8eee
LelieChristoph <christoph.lelie@bks.de>
2023-08-31 16:45:02 +0200

    InterruptLock zurueckgeändert

--- esphome/components/dallas/dallas_component.cpp
+++ esphome/components/dallas/dallas_component.cpp
@@ -162,17 +162,17 @@ const std::string &DallasTemperatureSensor::get_address_name() {
 bool IRAM_ATTR DallasTemperatureSensor::read_scratch_pad() {
   auto *wire = this->parent_->one_wire_;
 
-  InterruptLock lock;
- // {
-  //  InterruptLock lock;
+//  InterruptLock lock;
+  {
+    InterruptLock lock;
 
     if (!wire->reset()) {
       return false;
     }
- // }
+  }
 
-//  {
-  //  InterruptLock lock;
+  {
+    InterruptLock lock;
 
     wire->select(this->address_);
     wire->write8(DALLAS_COMMAND_READ_SCRATCH_PAD);
@@ -180,7 +180,7 @@ bool IRAM_ATTR DallasTemperatureSensor::read_scratch_pad() {
     for (unsigned char &i : this->scratch_pad_) {
       i = wire->read8();
     }
-//  }
+  }
 
   return true;
 }
